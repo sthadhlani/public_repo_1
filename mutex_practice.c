7d78a13e182e1260b97fa9cff4d51dd347df21e3
sthadhlani <sthadhlani@gmail.com>
2019-02-23 09:38:26 -0800

    Create mutex_practice.c

---
+++ mutex_practice.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <pthread.h>
+#define TOTAL_THREADS 3
+int i = 0;
+
+pthread_mutex_t shared_counter;
+
+void *thread_func(void *arg){
+   int j=0;
+   int x;
+   int *t_num = (int *)arg;
+   
+   do{
+   	pthread_mutex_lock(&shared_counter);
+	printf("This is parrellel thread %d iteration %d\n",*t_num,i++);
+	for(j=0;j<10000000;j++) {x = j;}
+	pthread_mutex_unlock(&shared_counter);
+   }while(i<100);
+   
+   return 0;
+} 
+
+
+int main(int argc, char* argv[]){
+    printf("Hello World!\n");
+    pthread_t thread_t[TOTAL_THREADS];
+    int x[TOTAL_THREADS];
+    int i = 0;
+    for(i = 0; i < TOTAL_THREADS; i++){
+    	x[i] = i;
+        if(pthread_create(&thread_t[i],NULL,thread_func,&x[i])){
+	     printf("Error creating thread\n");
+	     return 1;
+        }
+    }
+
+    for(i = 0; i < TOTAL_THREADS; i++){
+    	if(pthread_join(thread_t[i],NULL)){
+    		printf("Error Joinin Pthread %d",i);
+    	}
+    }
+    
+    return 0;
+}
